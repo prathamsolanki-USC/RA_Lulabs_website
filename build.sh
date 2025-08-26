6dcc262ae09155121935d6ecf1b8c421c5fa514c
prathamsolanki-USC <156559621+prathamsolanki-USC@users.noreply.github.com>
2025-08-25 22:18:05 -0700

    Complete cleanup - remove all Lambda files, clean App Runner config, direct Django install

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Starting build process..."
-pip install -r requirements.txt
-echo "Build completed!"
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Starting Django..."
-python manage.py collectstatic --noinput
-exec gunicorn api_website.wsgi:application --bind 0.0.0.0:8000 --workers 1
